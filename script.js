78c19e4b8f9fb53ef9d69b7f4c86ab260d711572
Linilya <liliandun2511@gmail.com>
2026-07-09 18:55:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+// Дата свадьбы
+const weddingDate = new Date("September 25, 2026 14:00:00").getTime();
+
+function updateTimer() {
+
+    const now = new Date().getTime();
+    const distance = weddingDate - now;
+
+    if (distance < 0) {
+
+        document.getElementById("days").innerHTML = "00";
+        document.getElementById("hours").innerHTML = "00";
+        document.getElementById("minutes").innerHTML = "00";
+        document.getElementById("seconds").innerHTML = "00";
+        return;
+
+    }
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) /
+        (1000 * 60 * 60)
+    );
+
+    const minutes = Math.floor(
+        (distance % (1000 * 60 * 60)) /
+        (1000 * 60)
+    );
+
+    const seconds = Math.floor(
+        (distance % (1000 * 60)) /
+        1000
+    );
+
+    document.getElementById("days").innerHTML =
+        String(days).padStart(2, "0");
+
+    document.getElementById("hours").innerHTML =
+        String(hours).padStart(2, "0");
+
+    document.getElementById("minutes").innerHTML =
+        String(minutes).padStart(2, "0");
+
+    document.getElementById("seconds").innerHTML =
+        String(seconds).padStart(2, "0");
+
+}
+
+updateTimer();
+setInterval(updateTimer, 1000);
+/* ---------- Появление блоков ---------- */
+
+/* ---------- Появление элементов ---------- */
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+
+    });
+
+},{
+    threshold:0.15
+});
+
+document.querySelectorAll(".fade-up").forEach(item=>{
+    observer.observe(item);
+});
